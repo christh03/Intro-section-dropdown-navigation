23114304795824c64a0300ad3bb54d3bb00ecec3
Chirsth Peralta <christhperalta@gmail.com>
2023-03-24 10:57:15 -0400

    Creating Provider and Header components

--- src/Provider/Provider.jsx
+++ src/Provider/Provider.jsx
@@ -1,68 +1,67 @@
 import { createContext } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-const GlobalContext = createContext();
+
+export const GlobalContext = createContext();
 
 const data = {
   header: {
     logo: { id: uuidv4(), text: 'snap', href: '#' },
 
-    nav: {
-      dropDownFeatures: {
-        Features: { id: uuidv4(), text: 'Features' },
-        list: [
-          {
-            id: uuidv4(),
-            text: 'Todo List',
-            href: '#',
-            svg: '/public/assets/images/icon-todo.svg',
-          },
-          {
-            id: uuidv4(),
-            text: 'Calendar',
-            href: '#',
-            svg: '/public/assets/images/icon-calendar.svg',
-          },
-          {
-            id: uuidv4(),
-            text: 'Reminders',
-            href: '#',
-            svg: '/public/assets/images/icon-reminders.svg',
-          },
-          {
-            id: uuidv4(),
-            text: 'Planning',
-            href: '#',
-            svg: '/public/assets/images/icon-planning.svg',
-          },
-        ],
-      },
-      dropDownCompany: {
-        Company: { id: uuidv4(), text: 'Features' },
-        list: [
-          {
-            id: uuidv4(),
-            text: 'History',
-            href: '#',
-          },
-          {
-            id: uuidv4(),
-            text: 'Our Team',
-            href: '#',
-          },
-          {
-            id: uuidv4(),
-            text: 'Blog',
-            href: '#',
-          },
-        ],
-      },
-
-      careers: { id: uuidv4(), text: 'Careers', href: '#' },
-      about: { id: uuidv4(), text: 'About', href: '#' },
-
-      login: { id: uuidv4(), text: 'Login', href: '#' },
-      register: { id: uuidv4(), text: 'Register' },
+    dropDownFeatures: {
+      button: { id: uuidv4(), text: 'Features' },
+      list: [
+        {
+          id: uuidv4(),
+          text: 'Todo List',
+          href: '#',
+          svg: '/assets/images/icon-todo.svg',
+        },
+        {
+          id: uuidv4(),
+          text: 'Calendar',
+          href: '#',
+          svg: '/assets/images/icon-calendar.svg',
+        },
+        {
+          id: uuidv4(),
+          text: 'Reminders',
+          href: '#',
+          svg: '/assets/images/icon-reminders.svg',
+        },
+        {
+          id: uuidv4(),
+          text: 'Planning',
+          href: '#',
+          svg: '/assets/images/icon-planning.svg',
+        },
+      ],
     },
+    dropDownCompany: {
+      button: { id: uuidv4(), text: 'Company' },
+      list: [
+        {
+          id: uuidv4(),
+          text: 'History',
+          href: '#',
+        },
+        {
+          id: uuidv4(),
+          text: 'Our Team',
+          href: '#',
+        },
+        {
+          id: uuidv4(),
+          text: 'Blog',
+          href: '#',
+        },
+      ],
+    },
+
+    careers: { id: uuidv4(), text: 'Careers', href: '#' },
+    about: { id: uuidv4(), text: 'About', href: '#' },
+
+    login: { id: uuidv4(), text: 'Login', href: '#' },
+    register: { id: uuidv4(), text: 'Register' },
   },
 
   main: {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,135 @@
-import React from 'react';
+import { useContext, useState } from 'react';
+import { GlobalContext } from '../../Provider/Provider';
 
 export const Header = () => {
   return (
     <>
       <header className="Header">
-        <h1 className="Header-h1"></h1>
-        <nav className="Header-nav">
-          <ul className="Header-ul">
-            <li className="Header-li">
-              <button className="Header-button"></button>
-            </li>
-            <li className="Header-li">
-              <button className="Header-button"></button>
-            </li>
-            <li className="Header-li">
-              <button className="Header-button"></button>
-            </li>
-          </ul>
-          <div className="Header-div">
-            <a href="#"></a>
-            <a href="#"></a>
-          </div>
-        </nav>
+        <Logo />
+        <Nav />
       </header>
     </>
+  );
+};
+
+export const Logo = () => {
+  const { header } = useContext(GlobalContext);
+  const { text } = header.logo;
+  return (
+    <>
+      <h1 className="Header-h1">{text}</h1>
+    </>
+  );
+};
+
+export const Nav = () => {
+  return (
+    <nav className="Header-nav">
+      <Menu />
+      <Registration />
+    </nav>
+  );
+};
+
+export const Menu = () => {
+  const { header } = useContext(GlobalContext);
+  const { careers } = header;
+  return (
+    <ul className="Header-ul">
+      <li className="Header-li">
+        <DropDownFeatures />
+      </li>
+      <li className="Header-li">
+        <DropDownCompany />
+      </li>
+      <li className="Header-li">
+        <a href={careers.href} className="Header-button">
+          {careers.text}
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export const Registration = () => {
+  const { header } = useContext(GlobalContext);
+  const { login, register } = header;
+  return (
+    <div className="Header-div">
+      <a href={login.href}>{login.text}</a>
+      <a href={register.href}>{register.text}</a>
+    </div>
+  );
+};
+
+export const DropDownFeatures = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { header } = useContext(GlobalContext);
+  const { button, list } = header.dropDownFeatures;
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <button className="Header-DopDownFeatures-button" onClick={handleToggle}>
+        {button.text}
+      </button>
+      {isOpen && (
+        <ul className="Header-DopDownFeatures-ul">
+          {list.map((item) => (
+            <DropDownFeaturesItem key={item.id} {...item} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export const DropDownFeaturesItem = ({ href, text, svg }) => {
+  return (
+    <li className="Header-DopDownFeatures-li">
+      <a className="Header-DopDownFeatures-a" href={href}>
+        <img className="Header-DopDownFeatures-img" src={svg} alt={text} />
+        {text}
+      </a>
+    </li>
+  );
+};
+
+export const DropDownCompany = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { header } = useContext(GlobalContext);
+  const { button, list } = header.dropDownCompany;
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <button className="Header-dropDownCompany-button" onClick={handleToggle}>
+        {button.text}
+      </button>
+
+      {isOpen && (
+        <ul className="Header-dropDownCompany-ul">
+          {list.map((item) => (
+            <DropDownCompanyItem key={item.id} {...item} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export const DropDownCompanyItem = ({ href, text }) => {
+  return (
+    <li className="Header-dropDownCompany">
+      <a href={href} className="Header-dropDownCompany-a">
+        {text}
+      </a>
+    </li>
   );
 };
